b2df9d96b71471c3c14fb16ca1b1444016d246d2
Daniel <danielbekool@gmail.com>
2025-05-05 18:24:52 +0700

    Update: add carouel fasilitas

---
+++ src/swiper.js
@@ -0,0 +1,28 @@
+const swiper = new Swiper('.swiper', {
+    loop: true, // Aktifkan infinite loop
+    spaceBetween: 20,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    autoplay: {
+      delay: 3000, 
+      disableOnInteraction: false, 
+    },
+    breakpoints: {
+      1024: {
+        slidesPerView: 3,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      0: {
+        slidesPerView: 1,
+      },
+    },
+  });
+  
